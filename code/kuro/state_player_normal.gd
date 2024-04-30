1b874d6d09e612d4c52f087739b0d0417a31b603
Violet Kurtz <vi.kurtz@protonmail.com>
2024-04-30 15:33:46 -0700

    Improved direction picking for controllers

--- code/kuro/state_player_normal.gd
+++ code/kuro/state_player_normal.gd
@@ -2,10 +2,14 @@ class_name StatePlayerNormal
 extends StatePlayer
 
 const WALK_SPEED: float = 100.0
+const STRING_LEFT: String = "left"
+const STRING_RIGHT: String = "right"
+const STRING_UP: String = "up"
+const STRING_DOWN: String = "down"
 
 @export var sprite: AnimatedSprite2D = null
 
-var _direction_string: String = "down"
+var _direction_string: String = STRING_DOWN
 
 
 func _update( _dt: float ) -> void:
@@ -21,15 +25,20 @@ func _update( _dt: float ) -> void:
 		var down_speed = move_vec.dot( Vector2.DOWN )
 		var max_speed = max( left_speed, right_speed, up_speed, down_speed )
 		
-		# Preferentially pick left/right
-		if max_speed == left_speed:
-			_direction_string = "left"
-		elif max_speed == right_speed:
-			_direction_string = "right"
-		elif max_speed == up_speed:
-			_direction_string = "up"
-		elif max_speed == down_speed:
-			_direction_string = "down"
+		# Only update if the direction has changed/if prior direction is no longer appropriate
+		if (_direction_string == STRING_LEFT and left_speed < max_speed) \
+				or (_direction_string == STRING_RIGHT and right_speed < max_speed) \
+				or (_direction_string == STRING_UP and up_speed < max_speed) \
+				or (_direction_string == STRING_DOWN and down_speed < max_speed):
+			# Preferentially pick left/right
+			if max_speed == left_speed:
+				_direction_string = STRING_LEFT
+			elif max_speed == right_speed:
+				_direction_string = STRING_RIGHT
+			elif max_speed == up_speed:
+				_direction_string = STRING_UP
+			elif max_speed == down_speed:
+				_direction_string = STRING_DOWN
 
 	# Play animations
 	if move_vec.length_squared() > 0.0:
