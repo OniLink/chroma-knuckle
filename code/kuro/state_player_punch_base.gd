6e77f45327f9ffba5a7373e2071eab14e1c425c4
catgirlreal <168145982+catgirlreal@users.noreply.github.com>
2024-05-06 19:00:23 -0400

    Update state_player_punch_base.gd

--- code/kuro/state_player_punch_base.gd
+++ code/kuro/state_player_punch_base.gd
@@ -3,3 +3,6 @@ extends StatePlayer
 
 const PUNCH_RHYTHM: float = 0.5
 const PUNCH_WIGGLE: float = 0.1
+
+func _start( _params : Dictionary = {} ) -> void:
+	get_controller().velocity = Vector2.ZERO
